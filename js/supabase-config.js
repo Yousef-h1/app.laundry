8414cc6d8c5c815252e3b7ec95aba2ae6f09301c
Yousef-h1 <Yousef_h@live.com>
2026-09-08 20:53:41 +0300

    Add files via upload

---
+++ js/supabase-config.js
@@ -0,0 +1,1 @@
+window.SUPABASE = { url: 'https://cgnkhwccolynhwrudthj.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnbmtod2Njb2x5bmh3cnVkdGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NjQyNjYsImV4cCI6MjEwNDQ0MDI2Nn0.fBcyygtFYfGGCS7T4pVscpdEZ6CPwurVU_A9IWcFhus' };
