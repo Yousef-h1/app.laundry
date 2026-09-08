dc915337c4fb325a804099e8fe6305222654358b
Yousef-h1 <Yousef_h@live.com>
2026-09-08 21:33:25 +0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,4 @@
 window.SUPABASE = { 
     url: 'https://cgnkhwccolynhwrudthj.supabase.co', 
-    anonKey: 'sb_publishable_Ky4BAKeseP3EJU440nEI1A_QsyFDBi7
-' 
+    anonKey: 'sb_publishable_Ky4BAKeseP3EJU440nEI1A_QsyFDBi7' 
 };
